fbd2922fe15a07650846a9db2286d100d09021a4
Nikki A <nikki.apostolakis@gmail.com>
2018-11-01 02:19:53 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -43,7 +43,7 @@ class PetsController < ApplicationController
       @pet.owner_id = @owner.id
       @pet.save
     end
-    binding pry
+    binding.pry
     redirect to "pets/#{@pet.id}"
   end
 end
