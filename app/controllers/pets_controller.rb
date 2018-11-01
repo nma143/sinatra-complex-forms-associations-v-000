d4adfd6133c32dd06de9241b3ddd2abba26f9b97
Nikki A <nikki.apostolakis@gmail.com>
2018-11-01 02:18:56 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -32,7 +32,9 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do
-
+    #if !params[:pet].keys.include?("owner_id")
+    #  params[:pet]["owner_id"] = []
+    #end
 
     @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
